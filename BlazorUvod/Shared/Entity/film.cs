35e076bfae402eab6e79ad38859cf8fa1aaaaf42
Silvester Markovič <silvester@tomas.si>
2022-01-05 20:07:07 +0100

    Add project files.

---
+++ BlazorUvod/Shared/Entity/film.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorUvod.Shared.Entity
+{
+    public class film
+    {
+        public string naziv;
+        public string slika;
+        public string opis;
+    }
+}
